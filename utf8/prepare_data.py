dfc845047e7ed2799686bcd3594b459ad9e9f735
Leo Rocha <leo.m.rocha@gmail.com>
2020-01-31 12:40:05 +0100

    starting to prepare models and data separation according to string length

---
+++ utf8/prepare_data.py
@@ -0,0 +1,102 @@
+"""
+File that prepares a train, test, dev dataset separation per different max string lengths
+
+The outputs will be
+"""
+from collections import OrderedDict
+import itertools
+from multiprocessing import Pool, cpu_count
+import numpy as np
+import os
+import orjson as json
+
+try:
+    from .utils import *
+except:
+    # hack to solve issue with ipython executing this import
+    from utils import *
+
+
+def get_txt2txtfiles(basepaths=[], filter_txt="txt2txt.json"):
+    """
+    Gets the list of all the files
+    :param basepaths: an iterable of paths to look for files
+    :param filter_txt: the text string that must be present in the filename
+    :return: a (flattened) list of absolute paths to all the found files
+    """
+    files_lists = [get_all_files_recurse(bp) for bp in basepaths]
+    all_files = [f for fl in files_lists for f in fl if filter_txt in f]
+    return all_files
+
+
+# MAX_LENS = (0, 64, 128, 256, 384, 512, 768, 1024, 2048)
+MAX_LENS = (0, 256, 384, 512, 768, 1024, 2048)
+
+
+def _group_foo(x, arr=np.array(MAX_LENS)):
+    ml = max(len(x["input"]), len(x["target"]))
+    am = np.argmax(arr > ml)
+    return arr[am]
+
+
+def separate_by_strlen(fname, max_lens=MAX_LENS):
+    """
+    Separates a single file into multiple ones with different string lengths
+    The function assumes the input is a json file AND the json format contains:
+    {'input': "[TEXT]",
+     'target': "[TEXT]"
+     }
+     and the filename contains the task at hand
+    :param fname: absolute path for the file to process
+    :param max_lens: a list of the max length of each string for each file
+    :return: will write a list of files separating by max(input|output) length of the task in place,
+
+    """
+    with json.loads(open(fname, 'rb').read()) as f:
+        groups = {}
+        uniquekeys = set([])
+        for k, g in itertools.groupby(f, _group_foo):
+            if k in groups:
+                groups[k].extend(list(g))
+            else:
+                groups[k] = list(g)
+            uniquekeys.add(k)
+        groups = OrderedDict(sorted(groups.items()))
+        # now save the files
+        for k, v in groups.items():
+            oname = fname.replace(".json", "max-{}.json".format(k))
+            with open(oname, "wb") as of:
+                txt = json.dumps(v)
+                of.write(txt)
+                of.flush()
+
+
+def _try_process(fname):
+    try:
+        separate_by_strlen(fname)
+    except Exception as e:
+        print("Error processing file: {} \nWith error: {}".format(fname, e))
+
+
+# TODO make this configurable instead
+UD_VERSION = "2.5"
+BASEPATH = "/home/leo/projects/Datasets/text"
+# BASEPATH = "/media/nfs/Datasets/text"
+CONLLU_BASEPATH = os.path.join(BASEPATH, 'UniversalDependencies/ud-treebanks-v{}'.format(UD_VERSION))
+GLUE_BASEPATH = os.path.join(BASEPATH, "GLUE")
+SUPERGLUE_BASEPATH = os.path.join(BASEPATH, "SuperGLUE")
+WIKIMATRIX_BASEPATH = "/media/nfs/Datasets/text/WikiMatrix/v1"
+
+PATHS = [CONLLU_BASEPATH, GLUE_BASEPATH, SUPERGLUE_BASEPATH, WIKIMATRIX_BASEPATH]
+
+
+def process(paths=PATHS):
+
+    all_files = get_txt2txtfiles(paths)
+
+    with Pool(processes=cpu_count()-1) as pool:
+        res = pool.map(_try_process, all_files)
+
+
+if __name__ == "__main__":
+    process()
--- utf8/trainer_helpers.py
+++ utf8/trainer_helpers.py
@@ -111,10 +111,10 @@ def loss_txt2txt_single(prediction, target,
 # optimizer = ...
 # model, optimizer = amp.initialize(model, optimizer,
 #                                       opt_level=args.opt_level,
-#                                       keep_batchnorm_fp32=args.keep_batchnorm_fp32,
-#                                       loss_scale=args.loss_scale
+#                                       keep_batchnorm_fp32=True,
+#                                       loss_scale="dynamic"
 #                                       )
-# model, optimizer = amp.initialize(model, optimizer, opt_level="O1")  # [O0, O1, O2, O3 ]
+# model, optimizer = amp.initialize(model, optimizer, opt_level="O2")  # [O0, O1, O2, O3 ]
 # for data, label in data_iter:
 #     out = model(data)
 #     loss = criterion(out, label)
@@ -123,10 +123,21 @@ def loss_txt2txt_single(prediction, target,
 #         scaled_loss.backward()
 #     optimizer.step()
 
+def train_main(model, optimizer, criterion, data_loader,
+               opt_level="O2", keep_batchnorm_fp32=True, loss_scale="dynamic"
+               ):
+
+    model, optimizer = amp.initialize(model, optimizer, opt_level=opt_level,  # [O0, O1, O2, O3 ]
+                                      keep_batchnorm_fp32=keep_batchnorm_fp32,
+                                      loss_scale=loss_scale)
+
+
+    pass
+
 
 def train_epoch(model, optimizer, criterion, data_iterator, epoch, summary_writer, total_batch_count=0):
     """
-    
+
     :param model:
     :param optimizer:
     :param criterion:
@@ -136,6 +147,7 @@ def train_epoch(model, optimizer, criterion, data_iterator, epoch, summary_write
     :param total_batch_count:
     :return:
     """
+    torch.cuda.empty_cache()
     train_loss = 0
     batch_idx = 1
     names = ['pred_loss', 'orig_lang_loss', 'dest_lang_loss', 'task_lang_loss', 'task_desc_loss']
@@ -168,163 +180,4 @@ def train_epoch(model, optimizer, criterion, data_iterator, epoch, summary_write
     print('====> Timestamp {} Epoch: {} Average loss: {:.8f}'.format(datetime.now(), epoch, train_loss / batch_idx))
     return batch_idx + total_batch_count
 
-
-def train(model, optimizer, loss_function, batches, epoch, ndatapoints, device, max_seq_len=-1):
-    torch.cuda.empty_cache()
-    model.train()
-    # TODO set the non trainable parameters if needed!!
-    train_loss = 0
-    #     batch_loss = []
-    batch_idx = 1
-    for b_data in batches:
-        b_train = torch.from_numpy(b_data[:, 0, :].astype("int32")).squeeze().to(device)
-        # max_seq_len means I'll take care only of a part of the input to compare output. This is to save computation
-        # only and not because of any other reason
-        if max_seq_len > 0:
-            b_upos = torch.from_numpy(b_data[:, 1, :max_seq_len].astype("int32")).squeeze().to(device)
-            b_deprel = torch.from_numpy(b_data[:, 2, :max_seq_len].astype("int32")).squeeze().to(device)
-        else:
-            b_upos = torch.from_numpy(b_data[:, 1, :].astype("int32")).squeeze().to(device)
-            b_deprel = torch.from_numpy(b_data[:, 2, :].astype("int32")).squeeze().to(device)
-        #
-        optimizer.zero_grad()
-        dec = model(b_train)
-        # last_latent = latent[-1]
-        upos, deprel = dec
-        # print(emb.shape,emb.dtype, res.shape, res.dtype)
-        # print(upos.shape, b_upos.shape)
-        # loss = loss_function(upos, deprel, upos_emb(b_upos), deprel_emb(b_deprel))
-        # print("train tensor shapes: ", b_train.shape, upos.shape, b_upos.shape, deprel.shape, b_deprel.shape)
-        loss, upos_loss, deprel_loss = loss_function(upos.view([-1, 18]), deprel.view([-1, 278]), b_upos.view([-1]), b_deprel.view([-1]))
-
-        loss.backward()
-        train_loss += loss.data.item()  # [0]
-        writer.add_scalar("Loss/train", loss.data.item(), global_step=(epoch * batch_idx))
-        writer.add_scalar("Loss/train/upos", upos_loss.data.item(), global_step=(epoch * batch_idx))
-        writer.add_scalar("Loss/train/deprel", deprel_loss.data.item(), global_step=(epoch * batch_idx))
-        optimizer.step()
-        batch_idx += 1
-        del b_train
-        del b_upos
-        del b_deprel
-        torch.cuda.empty_cache()
-    writer.add_scalar("EpochLoss/train", train_loss / batch_idx, epoch)
-    print('====> Timestamp {} Epoch: {} Average loss: {:.8f}'.format(datetime.now(), epoch, train_loss / ndatapoints))
-    return train_loss
-
-
-def test(model, loss_function, test_data, epoch, device, max_data=40, max_seq_len=-1):
-    """
-
-    :param model:
-    :param loss_function:
-    :param test_data:
-    :param epoch:
-    :param device:
-    :param max_data: maximum amout of data to test (default 50 due to gpu memory constraints in my pc)
-    :return:
-    """
-    model.eval()
-    test_loss = 0
-    for lang, d in test_data:
-        torch.cuda.empty_cache()  # make sure the cache is emptied to begin the nexxt batch
-        b_test = torch.from_numpy(d[:max_data, 0, :].astype("int32")).squeeze().to(device)
-        if max_seq_len > 0:
-            b_upos = torch.from_numpy(d[:max_data, 1, :max_seq_len].astype("int32")).squeeze().to(device).int()  # 
-            b_deprel = torch.from_numpy(d[:max_data, 2, :max_seq_len].astype("int32")).squeeze().to(device).int()  # 
-        else:
-            b_upos = torch.from_numpy(d[:max_data, 1, :].astype("int32")).squeeze().to(device).int()  # 
-            b_deprel = torch.from_numpy(d[:max_data, 2, :].astype("int32")).squeeze().to(device).int()  # 
-        upos, deprel = model(b_test)
-        # loss = loss_function(upos.view([-1, 18]), b_upos.view([-1]))
-        # print("test tensor shapes: ", b_test.shape, upos.shape, b_upos.shape, deprel.shape, b_deprel.shape)
-        loss, upos_loss, deprel_loss = loss_function(upos.view([-1, 18]), deprel.view([-1, 278]), b_upos.view([-1]), b_deprel.view([-1]))
-        test_loss += loss.data.item()
-        writer.add_scalar("LangLoss/test/"+lang, loss.data.item(), global_step=epoch)
-        writer.add_scalar("LangLoss/test/upos/"+lang, upos_loss.data.item(), global_step=epoch)
-        writer.add_scalar("LangLoss/test/deprel/"+lang, deprel_loss.data.item(), global_step=epoch)
-        del b_test
-        del b_upos
-        del b_deprel
-        torch.cuda.empty_cache()
-    test_loss /= len(test_data)  # although this is not faire as different languages give different results
-    writer.add_scalar("EpochLangLoss/test/", test_loss, global_step=epoch)
-    print('epoch: {}====> Test set loss: {:.8f}'.format(epoch, test_loss))
-
-
-def test_accuracy(model, test_data, epoch, device, max_data=50):
-    torch.cuda.empty_cache()  # make sure the cache is emptied
-    model.eval()
-    epoch_acc = 0
-
-    upos_eye = torch.eye(len(UPOS))
-    deprel_eye = torch.eye(len(DEPREL))
-    with torch.no_grad():
-        upos_emb = nn.Embedding(*upos_eye.shape)
-        upos_emb.weight.data.copy_(upos_eye)
-        upos_emb = upos_emb.to(device)
-
-        deprel_emb = nn.Embedding(*deprel_eye.shape)
-        deprel_emb.weight.data.copy_(deprel_eye)
-        deprel_emb.to(device)
-
-    for lang, d in test_data:
-        with torch.no_grad():
-            b_test = torch.from_numpy(d[:max_data, 0, :].astype("int32")).squeeze().to(device)
-            # TODO move the testing part to CPU so it takes less memory in the GPU and can keep training while testing
-            # doing operations in boolean form so it takes less space in gpu
-            b_upos = torch.from_numpy(d[:max_data, 1, :].astype("bool")).squeeze().to(device)
-            b_deprel = torch.from_numpy(d[:max_data, 2, :].astype("bool")).squeeze().to(device)
-            _, _, _, dec = model(b_test)
-            #         last_latent = latent[-1]
-            upos, deprel = dec
-            ones = torch.ones(1).to(device)
-            zeros = torch.zeros(1).to(device)
-            upos = torch.where(upos > 0.9, ones, zeros).bool().to(device)
-            deprel = torch.where(deprel > 0.9, ones, zeros).bool().to(device)
-            upos = upos.view([-1, 18])
-            deprel = deprel.view([-1, 278])
-
-        # FIXME this accuracy measurement does not work.
-        upos_acc = (upos == upos_emb(b_upos).view([-1, 18])).sum().item() / upos.shape[0]
-        deprel_acc = (deprel == deprel_emb(b_deprel).view([-1, 278])).sum().item() / deprel.shape[0]
-        acc = (upos_acc + deprel_acc) / 2
-        # print("accuracy : ", acc, upos_acc, deprel_acc)
-        writer.add_scalar("LangAccuracy/test/" + lang, acc, global_step=epoch)
-        writer.add_scalar("LangAccuracy/test/upos/" + lang, upos_acc, global_step=epoch)
-        writer.add_scalar("LangAccuracy/test/deprel/" + lang, deprel_acc, global_step=epoch)
-
-        del b_test
-        del b_upos
-        del b_deprel
-        torch.cuda.empty_cache()
-    epoch_acc /= len(test_data)  # although this is not faire as different languages give different results
-    writer.add_scalar("EpochLangAccuracy/test/", epoch_acc, global_step=epoch)
-    print('epoch: {}====> Test Accuracy set loss: {:.4f}'.format(epoch, acc))
-    pass
-
-
-def load_test_data(base_dir, max_samples=-1, max_seq_len=-1):
-    """finds all ud-treebank data that was pre-processed and saved in numpy and loads it.
-    Each file is loaded and kept in a tuple (lang, dataset) and returns a list of those values
-    if max_samples or max_seq_len are set to a nunmber greater than zero these will limit the data returned
-    """
-    # load testing data ALL the training data
-
-    # get all file paths for testing
-    all_fnames = get_all_files_recurse(base_dir)
-    fnames = [f for f in all_fnames if "test-charse" in f and f.endswith(".npy")]
-    # load all test files
-    test_data = []
-    for f in fnames:
-        data = np.load(f)
-        # data is shape: [total samples, data channels (3), 1024]
-        # print("data loading shape: ", data.shape)
-        if max_seq_len > 0:
-            data = data[:, :, :max_seq_len]
-        if max_samples > 0:
-            data = data[:max_samples, :, :]
-        lang_name = path_leaf(f).split("-ud")[0]
-        test_data.append((lang_name, data))
-    return test_data
 
