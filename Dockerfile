86d6f7795121467965c97d4a78141c35bda5cd5e
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2021-01-09 18:43:30 +0700

    Adds chartify dependency.

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM jupyter/datascience-notebook:703d8b2dcb88
+
+RUN pip install pip install chartify
--- run-jupyter.sh
+++ run-jupyter.sh
@@ -2,5 +2,5 @@
 # list of images here https://jupyter-docker-stacks.readthedocs.io/en/latest/using/selecting.html
 # exmamples of scripts here https://jupyter-docker-stacks.readthedocs.io/en/latest/index.html
 
-
-docker run --rm --name mix_nlp_jupyter -p 8888:8888 -v "$PWD":/home/jovyan jupyter/datascience-notebook:703d8b2dcb88
+docker build -t mix_nlp_jupyter .
+docker run --rm --name mix_nlp_jupyter -p 8888:8888 -v "$PWD":/home/jovyan mix_nlp_jupyter
