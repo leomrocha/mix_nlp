3b8eab006f9aa077856eab9fa0eb9dca5d060a2b
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2021-01-26 15:32:00 +0700

    gutenberg aggregate by author

--- utf8/postprocessors/gutenberg_aggregate.py
+++ utf8/postprocessors/gutenberg_aggregate.py
@@ -14,11 +14,19 @@ except:
 import sqlite3
 
 
-def get_path_and_dict(stats):
-    first_value = next(iter(mydict.values()))
-    if type(first_value) == int:
-        pass
+LANGUAGES_DIR = 'gutenberg_stats/clean_results'
+TARGET_DIR = 'gutenberg_stats_short/aggregate'
 
+# uncomment to include by word statistics, will not be committed
+# TARGET_DIR = 'gutenberg_stats/aggregate'
+
+
+def main():
+    # aggregate_sample()
+    aggregate_by_language()
+
+    if not 'short' in TARGET_DIR:
+        aggregate_by_author()
 
 
 def sum_stats(data_source_path):
@@ -41,14 +49,13 @@ def sum_stats(data_source_path):
             'token_length': pd.Series(dtype=int), 
             'char_length': pd.Series(dtype=int),
         }, 
-        # uncomment this to include words statistics, 
-        # will result in larger files (do not commit them) and 
-        # the computation will be slower 
-        # (as pandas does some aligning on the index https://pandas.pydata.org/pandas-docs/stable/user_guide/dsintro.html?highlight=alignment#vectorized-operations-and-label-alignment-with-series)
-        # 'by_word': {
-        #     'words': pd.Series(dtype=int)
-        # }
     }
+
+    # will result in larger files and the computation will be slower 
+    # as pandas does some aligning on the index 
+    # https://pandas.pydata.org/pandas-docs/stable/user_guide/dsintro.html?highlight=alignment#vectorized-operations-and-label-alignment-with-series)
+    if not 'short' in TARGET_DIR:
+        aggregated_stats['by_word'] = {'words': pd.Series(dtype=int)}
 
     # get all files (including sub directories)
     paths = list(Path(data_source_path).rglob("*stats_all*.json.gz"))
@@ -83,6 +90,7 @@ def sum_stats(data_source_path):
     return aggregated_stats
 
 
+# for manual testing purposes
 def aggregate_sample():
     sample_data_path = 'gutenberg_stats_examples'
     # print(sum_stats(sample_data_path)['doc'])
@@ -90,31 +98,44 @@ def aggregate_sample():
 
 
 def aggregate_by_language():
-    languages_dir = 'gutenberg_stats/clean_results'
-    languages = os.listdir(languages_dir)
+    languages = os.listdir(LANGUAGES_DIR)
 
     for language in languages:
         print(f'Aggregating by language: [{language}]')
-        aggregated_stats = sum_stats(f'{languages_dir}/{language}')
+        aggregated_stats = sum_stats(f'{LANGUAGES_DIR}/{language}')
         json_data = json.dumps(aggregated_stats)
 
-        target_dir = f'gutenberg_agg_stats/by_language'
+        target_dir = f'{TARGET_DIR}/by_language'
         os.makedirs(target_dir, exist_ok=True)
 
         with open(f'{target_dir}/{language}.json', 'w') as f:
             f.write(json_data)
 
 
+def aggregate_by_author():
+    languages = os.listdir(LANGUAGES_DIR)
+
+    for language in languages:
+
+        authors = os.listdir(f'{LANGUAGES_DIR}/{language}')
+
+        for author in authors:
+
+            print(f'Aggregating by language: [{language}], Author: [{author}]')
+            aggregated_stats = sum_stats(f'{LANGUAGES_DIR}/{language}/{author}')
+            json_data = json.dumps(aggregated_stats)
+
+            target_dir = f'{TARGET_DIR}/by_author/{language}/'
+            os.makedirs(target_dir, exist_ok=True)
+
+            with open(f'{target_dir}/{author}.json', 'w') as f:
+                f.write(json_data)
+
+
 def create_db():
     # TODO, all the rest
     #  for the moment I'm just starting to structure the DB ideas in the gutenberg_sql.py file
     pass
-
-
-
-def main():
-    # aggregate_sample()
-    aggregate_by_language()
 
 
 if __name__ == '__main__':
